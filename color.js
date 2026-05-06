81127354c55bfab0db58faadaa15d54af14fd72a
Sonal Vishwakarma <drsonalvishwakarma@gmail.com>
2026-05-06 14:35:39 +0530

    add

--- color.js
+++ color.js
@@ -1,2 +1,7 @@
 const colors = require('colors');
 console.log(colors.red("hello World"));
+console.log(colors.green("hello World"));
+console.log(colors.blue("hello World"));
+console.log(colors.yellow("hello World"));
+console.log(colors.cyan("hello World"));
+console.log(colors.magenta("hello Worlds.........................."));
--- server.js
+++ server.js
@@ -2,5 +2,12 @@ const http = require('http');
 http.createServer((req, resp)=>{
     // resp.write("Welcome to Node.js");
       resp.write("<h1>Welcome to Node.js</h1>");
-    resp.end("hello");
+    resp.end("hello....................................");
 }).listen(4800);
+
+
+
+http.createServer((res, resp)=>{
+  resp.write("<h1>Welcome to Node.js brbabbj</h1>");
+  // resp.end("Welcome to Node.jsfjj");
+}).listen(5900);
